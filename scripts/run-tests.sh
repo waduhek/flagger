ffe1dd21a2862005cd1fb24ad80c8f6a4e434619
Ryan Noronha <ryannor56a@proton.me>
2024-07-17 22:11:11 +0530

    build: wait for docker containers using `nc`

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -2,7 +2,9 @@
 
 # Name of the MongoDB container.
 mongo_container_name=mongo
+mongo_container_port=27017
 redis_container_name=redis
+redis_container_port=6379
 
 # Runs a docker container in detached mode.
 #
@@ -13,11 +15,27 @@ run_docker_container () {
     docker run --quiet --detach --name "$1" --publish "$2" "$3" >/dev/null 2>&1
 }
 
+# Gets the IP address of a container
+#
+# $1 Name given to the container.
+get_container_ip () {
+    docker inspect -f \{\{.NetworkSettings.IPAddress\}\} "$1"
+}
+
 # Waits for a container to change it's state to running.
 #
 # $1 Name given to the container.
+# $2 Port the container is running on.
 wait_for_container () {
-    until [ "$(docker inspect -f \{\{.State.Running\}\} "$1")" == "true" ] ; do
+    if ! command -v nc >/dev/null
+    then
+        echo "nc does not exist"
+        exit 1
+    fi
+
+    container_ip=$(get_container_ip "$1")
+
+    until nc -z "$container_ip" "$2" ; do
         sleep 0.1
     done
 }
@@ -30,12 +48,12 @@ force_stop_container () {
 }
 
 # Run MongoDB and Redis containers in detached mode and redirect all output to null.
-run_docker_container $mongo_container_name "27017:27017" mongo
-run_docker_container $redis_container_name "6379:6379" redis
+run_docker_container $mongo_container_name "$mongo_container_port:$mongo_container_port" mongo
+run_docker_container $redis_container_name "$redis_container_port:$redis_container_port" redis
 
 # Wait until the container is ready.
-wait_for_container $mongo_container_name
-wait_for_container $redis_container_name
+wait_for_container $mongo_container_name $mongo_container_port
+wait_for_container $redis_container_name $redis_container_port
 
 # Run tests.
 go list ./... | grep -Ev "cmd|proto" | tr "\n" " " | xargs go test -coverprofile cover.out
